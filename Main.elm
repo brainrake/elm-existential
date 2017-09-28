9e6df9b11a66bb44699a7634acd77320adeea5fa
Márton Boros <martonboros@gmail.com>
2017-09-28 03:37:05 +0200

    update

--- Main.elm
+++ Main.elm
@@ -12,21 +12,21 @@ import Html exposing (text)
 -- incTwiceCantUse : Counter Int -> Counter Int
 -- incTwiceCantUse c =
 --    inc c |> inc c
--- the only function you can ever write with a fully applied Counter
--- you can never use it since you can't construct a Counter Int
-
-
-notInteresting : Counter Int -> Counter Int
-notInteresting c =
-    c
-
-
-
 -- won't compile:  Hide a is not exported
 -- incTwiceCantUse : Counter (Hide Int) -> Counter (Hide Int)
 -- incTwiceCantUse c =
 --    inc c |> inc c
--- result : you can only write down useful function that use Counter if you leave the type variable abstract
+
+
+notInteresting : Counter Int -> Counter Int
+notInteresting c =
+    -- the only function you can ever write with a fully applied Counter
+    -- you can never use it since you can't construct a Counter Int
+    c
+
+
+
+-- result : you can only write down useful functions that use Counter if you leave the type variable abstract
 -- it's like existential types, but with explicit tracking of the unusable type variable
 
 
