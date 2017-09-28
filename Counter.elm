725aef8da8f2fe332a37946648105a0f1fc9caeb
Márton Boros <martonboros@gmail.com>
2017-09-28 03:35:34 +0200

    init

---
+++ Counter.elm
@@ -0,0 +1,43 @@
+module Counter exposing (Counter, mkCounter, zero, inc, val, intCounter, floatCounter)
+
+
+type Hide a
+    = Hide a
+
+
+type Counter a
+    = Counter { zero : a, inc : a -> a, val : a -> Int }
+
+
+zero : Counter (Hide a) -> Hide a
+zero (Counter c) =
+    c.zero
+
+
+inc : Counter (Hide a) -> (Hide a -> Hide a)
+inc (Counter c) =
+    c.inc
+
+
+val : Counter (Hide a) -> (Hide a -> Int)
+val (Counter c) =
+    c.val
+
+
+mkCounter : a -> (a -> a) -> (a -> Int) -> Counter (Hide a)
+mkCounter zero inc val =
+    Counter
+        { zero = Hide zero
+        , inc = \(Hide a) -> Hide (inc a)
+        , val = \(Hide a) -> val a
+        }
+
+
+intCounter : Counter (Hide Int)
+intCounter =
+    mkCounter 0 (\i -> i + 1) (\i -> i + 0)
+
+
+floatCounter : Counter (Hide Float)
+floatCounter =
+    mkCounter 0.0 (\i -> i + 1.0) round
---
+++ Main.elm
@@ -0,0 +1,40 @@
+module Main exposing (..)
+
+import Counter exposing (..)
+import Html exposing (text)
+
+
+-- won't compile:
+-- - inc only works with Counter (Hide a)
+-- wouldn't work anyway:
+-- - Can never have Counter Int
+-- - Hide a is not exported
+-- incTwiceCantUse : Counter Int -> Counter Int
+-- incTwiceCantUse c =
+--    inc c |> inc c
+-- the only function you can ever write with a fully applied Counter
+-- you can never use it since you can't construct a Counter Int
+
+
+notInteresting : Counter Int -> Counter Int
+notInteresting c =
+    c
+
+
+
+-- won't compile:  Hide a is not exported
+-- incTwiceCantUse : Counter (Hide Int) -> Counter (Hide Int)
+-- incTwiceCantUse c =
+--    inc c |> inc c
+-- result : you can only write down useful function that use Counter if you leave the type variable abstract
+-- it's like existential types, but with explicit tracking of the unusable type variable
+
+
+incTwice : Counter a -> Counter a
+incTwice c =
+    inc c |> inc c
+
+
+main : Html.Html msg
+main =
+    text (toString (zero intCounter |> incTwice intCounter |> val intCounter))
